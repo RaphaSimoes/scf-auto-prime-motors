d18719efc6d1600c77dfb9248cb25552c7c376f7
Luiz Gustavo <dantas.lgf@gmail.com>
2025-09-10 21:50:25 -0300

    projeto auto-motors CRUD de funcionarios

--- src/main/java/com/auto_prime/demo/config/Cors.java
+++ src/main/java/com/auto_prime/demo/config/Cors.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class Cors {
-    
+
     @Configuration
     public class CorsConfig {
         @Bean
